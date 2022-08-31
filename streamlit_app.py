7a757edf72d94e025f83e72cbab1027c41759f50
chakraguntur <52360829+chakraguntur@users.noreply.github.com>
2022-08-31 12:10:45 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -53,10 +53,10 @@ my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
 
-# my_cur.execute("insert into fruit_load_list values ('from streamlit)'")    
+# my_cur.execute("insert into fruit_load_list values ('from streamlit new_fruit variable)'")    
 def insert_row_snowflake(new_fruit):
     with my_cnx.cursor() as my_cur:
-        my_cur.execute("insert into pc_rivery_db.public.fruit_load_list values ('from streamlit')")
+        my_cur.execute("insert into pc_rivery_db.public.fruit_load_list values ('"+ new_fruit +"')")
         return "Thanks for adding " + new_fruit
     
 add_my_fruit=streamlit.text_input('Which fruit would you like to add?','Kiwi')
